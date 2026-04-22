e30462b88ca1f346c9989a37c9c8dd49682eb002
Sravik-Sajja <sravik.sajja@gmail.com>
2026-04-21 18:34:19 -0700

    Made bodyweight workouts show reps in graph

--- server/methods/weight_stats.py
+++ server/methods/weight_stats.py
@@ -25,12 +25,14 @@ def get_total_volume(data):
 def get_max_set_weight_progression(data):
     result = {}
     for sett in data:
+        if sett['weight'] == 0: weight = 1
+        else: weight = sett['weight']
         if sett['date'] in result:
             max_volume_of_that_day = result[sett['date']]
-            max_volume_of_current = sett['weight'] * sett['reps']
+            max_volume_of_current = weight * sett['reps']
             result[sett['date']] = max(max_volume_of_that_day, max_volume_of_current)
         else:
-            max_volume = sett['weight'] * sett['reps']
+            max_volume = weight * sett['reps']
             result[sett['date']] = max_volume
     return result
 
@@ -38,6 +40,8 @@ def get_average_set_progression(data):
     result = {}
     date_to_number = {}
     for sett in data:
+        if sett['weight'] == 0: weight = 1
+        else: weight = sett['weight']
         set_date = sett['date']
         if set_date in result:
             number_of_sets = date_to_number[set_date]
@@ -46,11 +50,11 @@ def get_average_set_progression(data):
             number_of_sets += 1
             date_to_number[set_date]+=1
             
-            total_sets_weight += sett['weight'] * sett['reps']
+            total_sets_weight += weight * sett['reps']
             average_sets_weight = total_sets_weight/number_of_sets
             result[set_date] = average_sets_weight
         else:
-            result[set_date] = sett['weight'] * sett['reps']
+            result[set_date] = weight * sett['reps']
             date_to_number[set_date] = 1
     return result
             
